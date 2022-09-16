13e0f0abcadf88a25b7f15b7bb754900437e60b3
Arnaud BODEL <contact@osw3.net>
2022-09-16 15:24:45 +0200

    -

--- 12-jQuery/app.js
+++ 12-jQuery/app.js
@@ -115,13 +115,16 @@ btn_vanilla_prop.addEventListener('click', event => {
 
     // Récup la hauteur en pixel de #vanilla
     console.log( _vanilla.clientHeight );
-
+    // Ne sais pas recup le padding facilement
 
     // Ajouter une valeur (SET)
     // --
     
     // Ajouter une class
     _vanilla.classList.add("my-new-vanilla-class");
+
+    // Modifier la hauteur de #vanilla
+    _vanilla.style.height = "120px";
 
 
 });
@@ -144,10 +147,25 @@ btn_jquery_prop.on('click', event => {
     console.log( _jQuery.height() );
     console.log( _jQuery.css('padding') );
 
+    // console.log( myJQueryDiv.attr('style', "background-color: green") );
+
     // Ajouter une valeur (SET)
     // --
 
     // Ajouter une class
     _jQuery.addClass("my-new-jquery-class");
 
+    _jQuery.height("120px");
+    // _jQuery.css("height", "120px");
+
 });
+
+
+
+// Syntaxe de jQuery
+// --
+
+// methode getter / setter
+
+// GET / Recupèrer une valeur : _jQuery.height();
+// SET / Affecter une valeur  : _jQuery.height("120px");
