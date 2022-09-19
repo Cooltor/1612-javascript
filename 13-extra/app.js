003e15a5d75371adfb78af9c236e408e9dd84de2
Arnaud BODEL <contact@osw3.net>
2022-09-19 10:25:51 +0200

    -

--- 13-extra/app.js
+++ 13-extra/app.js
@@ -1,16 +1,61 @@
 "use strict";
 
+const content       = document.getElementById('content');
+
+const mouse_pos_x   = document.getElementById('mouse_pos_x');
+const mouse_pos_y   = document.getElementById('mouse_pos_y');
+const scroll_pos_x  = document.getElementById('scroll_pos_x');
+const scroll_pos_y  = document.getElementById('scroll_pos_y');
+
 // Create many div in #content
 // --
 
 for (let i=0; i<200; i++)
 {
     // Create a <div>
-    let div = document.createElement('DIV');
+    const div = document.createElement('DIV');
 
     // Add "i" into the <div>
     div.innerText = i;
 
     // Put the <div> into the <#content>
-    document.getElementById('content').append(div);
+    content.append(div);
+}
+
+
+// Get Mouse positions
+// --
+
+document.onmousemove = event => {
+
+    // Get mouse positions
+    // const clientX = event.clientX;
+    // const clientY = event.clientY;
+
+    const {
+        clientX,
+        clientY
+    } = event;
+
+    // Set mouse positions
+    mouse_pos_x.innerText = clientX;
+    mouse_pos_y.innerText = clientY;
+}
+
+
+
+// Get Scroll positions
+// --
+
+document.onscroll = event => {
+
+    // Get scroll positions
+    const {
+        scrollX,
+        scrollY
+    } = window;
+
+    // Set scroll positions
+    scroll_pos_x.innerText = scrollX;
+    scroll_pos_y.innerText = scrollY;
 }
